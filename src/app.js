0ac3912846a75c6a77ae276f32117d9eb8d06b9d
Nathália Brum <natybrum.97@gmail.com>
2023-06-30 04:06:19 -0300

    Rota /participants pronta

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import Joi from "joi";
+import dayjs from 'dayjs';
 
 const app = express();
 
@@ -19,6 +21,48 @@ try {
 }
 
 const db = mongoClient.db();
+
+app.post("/participants", async(req,res) => {
+    const { name } = req.body;
+
+    const schemaUsuario = Joi.object({
+        name: Joi.string().required()
+	})
+
+    const validation = schemaUsuario.validate(req.body, { abortEarly: false });
+
+	if (validation.error) {
+		const errors = validation.error.details.map(detail => detail.message);
+		return res.status(422).send(errors);
+	}
+
+    try{
+        const usuario = await db.collection("participants").findOne({ name: name });
+        if (usuario) return res.status(409).send("Esse usuário já existe!");
+
+        await db.collection("participants").insertOne({name: req.body.name, lastStatus: Date.now()});
+
+        const data = dayjs();
+
+        const horaFormatada = data.format('HH:mm:ss');
+
+        await db.collection("messages").insertOne({from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: horaFormatada});
+
+		res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
+})
+
+app.get("/participants", async (req, res) => {
+	try {
+		const participantes = await db.collection("participants").find().toArray();
+		res.send(participantes);
+	} catch (err) {
+		res.status(500).send(err.message)
+	}
+})
 
 const PORT = 5000;
 
