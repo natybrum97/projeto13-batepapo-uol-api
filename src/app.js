f2a83a334e11e36285cc989a53b8fffbb6a90d34
Nathália Brum <natybrum.97@gmail.com>
2023-06-30 06:35:02 -0300

    Versão atualizada

--- src/app.js
+++ src/app.js
@@ -14,41 +14,41 @@ dotenv.config();
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
-	await mongoClient.connect();
-	console.log("MongoDB conectado!");
+    await mongoClient.connect();
+    console.log("MongoDB conectado!");
 } catch (err) {
-	(err) => console.log(err.message);
+    (err) => console.log(err.message);
 }
 
 const db = mongoClient.db();
 
-app.post("/participants", async(req,res) => {
+app.post("/participants", async (req, res) => {
     const { name } = req.body;
 
     const schemaUsuario = Joi.object({
         name: Joi.string().required()
-	})
+    })
 
     const validation = schemaUsuario.validate(req.body, { abortEarly: false });
 
-	if (validation.error) {
-		const errors = validation.error.details.map(detail => detail.message);
-		return res.status(422).send(errors);
-	}
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    }
 
-    try{
+    try {
         const usuario = await db.collection("participants").findOne({ name: name });
         if (usuario) return res.status(409).send("Esse usuário já existe!");
 
-        await db.collection("participants").insertOne({name: req.body.name, lastStatus: Date.now()});
+        await db.collection("participants").insertOne({ name: req.body.name, lastStatus: Date.now() });
 
         const data = dayjs();
 
         const horaFormatada = data.format('HH:mm:ss');
 
-        await db.collection("messages").insertOne({from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: horaFormatada});
+        await db.collection("messages").insertOne({ from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: horaFormatada });
 
-		res.sendStatus(201);
+        res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
     }
@@ -56,12 +56,76 @@ app.post("/participants", async(req,res) => {
 })
 
 app.get("/participants", async (req, res) => {
-	try {
-		const participantes = await db.collection("participants").find().toArray();
-		res.send(participantes);
-	} catch (err) {
-		res.status(500).send(err.message)
-	}
+    try {
+        const participantes = await db.collection("participants").find().toArray();
+        res.send(participantes);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body;
+    const user = req.headers.user;
+
+    const schemaMessage = Joi.object({
+        to: Joi.string().required(),
+        text: Joi.string().required(),
+        type: Joi.string().valid('message', 'private_message').required()
+    })
+
+    const validation = schemaMessage.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const usuario = await db.collection("participants").findOne({ name: user });
+        if (!usuario) return res.status(401).send("Esse usuário não está na lista de participantes! Faça o Login novamente.");
+
+        const data = dayjs();
+
+        const horario = data.format('HH:mm:ss');
+
+        await db.collection("messages").insertOne({ from: user, to: to, text: text, type: type, time: horario });
+
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
+})
+
+app.get("/messages", async (req, res) => {
+
+    const limit = parseInt(req.query.limit);
+
+    const user = req.headers.user;
+
+    try {
+        const mensagens = await db.collection("messages").find({
+            $or: [
+                { to: "Todos" },
+                { to: user },
+                { from: user }
+            ]
+        }).toArray();
+
+        if (limit) {
+            if (typeof limit === 'number' && limit > 0 && Number.isInteger(limit)) {
+              return res.send(mensagens.slice(-limit));
+            } else {
+              return res.status(422).send("Limite inválido. Certifique-se de que é um número inteiro positivo.");
+            }
+          }
+
+        res.send(mensagens);
+        
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 })
 
 const PORT = 5000;
